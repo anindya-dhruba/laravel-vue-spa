a4ecb7638bccc90719ffc7716adfbeb9364bc64a
Unknown <flefloch@naoc.fr>
2019-03-18 11:42:17 +0100

    [Back] Add Broadcast through Pusher API

---
+++ app/Events/MessageSent.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Events;
+
+use App\User;
+use App\Message;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class MessageSent implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+     /**
+     * User that sent the message
+     *
+     * @var User
+     */
+    public $user;
+
+    /**
+     * Message details
+     *
+     * @var Message
+     */
+    public $message;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user, Message $message)
+    {
+        $this->user = $user;
+        $this->message = $message;
+    }
+
+    /**
+     * Broadcast new Event.
+     *
+     * @return void
+     */
+    /*public function broadcastWith()
+    {
+        $this->message->load(['user']);
+
+        return [
+            'MessageSent' => $this->message->toArray()
+        ];
+    }*/
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('chat');
+    }
+}
--- app/Providers/BroadcastServiceProvider.php
+++ app/Providers/BroadcastServiceProvider.php
@@ -14,7 +14,7 @@ class BroadcastServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Broadcast::routes();
+        Broadcast::routes(['middleware' => ['jwt.auth']]);
 
         require base_path('routes/channels.php');
     }
